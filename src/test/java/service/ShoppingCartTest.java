74cdc57390f308bb677d6dc42e4fe4f1bc621ce2
sihle <sihle@zailab.com>
2019-07-05 19:15:33 +0200

    Step 2: Add additional products of the same type to the shopping cart.

--- src/test/java/service/ShoppingCartTest.java
+++ src/test/java/service/ShoppingCartTest.java
@@ -7,10 +7,11 @@ import org.junit.Test;
 import static util.Formatter.formatPrice;
 
 public class ShoppingCartTest {
+    private ShoppingCart shoppingCart;
 
     @Test
     public void testAddProduct() {
-        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart = new ShoppingCart();
         Product product = new Product("Dove", 39.99);
 
         shoppingCart.addProduct(product);
@@ -21,7 +22,30 @@ public class ShoppingCartTest {
 
         Assert.assertEquals(5, shoppingCart.getProducts().size());
         assertProductPrice(shoppingCart);
-        Assert.assertTrue(formatPrice(199.95) == shoppingCart.calculatePrice());
+        Assert.assertTrue(199.95 == shoppingCart.calculatePrice());
+    }
+
+    @Test
+    public void testAddSameProductType() {
+        shoppingCart = new ShoppingCart();
+        Product product = new Product("Dove", 39.99);
+
+        shoppingCart.addProduct(product);
+        shoppingCart.addProduct(product);
+        shoppingCart.addProduct(product);
+        shoppingCart.addProduct(product);
+        shoppingCart.addProduct(product);
+
+        Assert.assertEquals(5, shoppingCart.getProducts().size());
+        assertProductPrice(shoppingCart);
+
+        shoppingCart.addProduct(product);
+        shoppingCart.addProduct(product);
+        shoppingCart.addProduct(product);
+
+        Assert.assertEquals(8, shoppingCart.getProducts().size());
+        assertProductPrice(shoppingCart);
+        Assert.assertTrue(319.92 == shoppingCart.calculatePrice());
     }
 
     private void assertProductPrice(ShoppingCart shoppingCart) {
